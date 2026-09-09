8826cba9647316592a3d1ea4a2b8f0eadb3bade2
Astral <132731805+astralsc@users.noreply.github.com>
2026-09-10 07:56:00 +0800

    pubnub

--- basic/notification.php
+++ basic/notification.php
@@ -3,13 +3,13 @@ http_response_code(200);
 header('Content-Type: application/json');
 
 $data = [
-    'subscribeKey' => 'demo',
-    'gameNotificationChannel' => 'game-notify-demo',
-    'gameGlobalNotificationChannel' => 'game-global-demo',
-    'playerChannel' => 'player-demo-001',
-    'playerForRealmChannel' => 'player-realm-demo',
+    'subscribeKey' => 'sub-c-3ae5cf75-8ff9-4317-93dd-a4c5bc5d29b2',
+    'gameNotificationChannel' => 'game-notify-DE_1402027310012443',
+    'gameGlobalNotificationChannel' => 'game-global-DE_1402027310012443',
+    'playerChannel' => 'player-71880669866436610',
+    'playerForRealmChannel' => 'realm-DE_1402027310012443',
     'customChannelPrefix' => 'custom-',
-    'authenticationKey' => ''
+    'authenticationKey' => 'sec-c-NTUzYTcxZjAtZjg2OS00MjgyLThkMGUtZTQ4Y2E4ZjE3OGE2'
 ];
 
 echo json_encode($data);
